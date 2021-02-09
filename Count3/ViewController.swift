4062d66c1e278d176ae91d40b21af1fcbc2963d3
rukanaction <ruka.34ever57@gmail.com>
2021-02-09 13:43:30 +0900

    カウントアプリ3の完成

--- Count3/ViewController.swift
+++ Count3/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func multiply(){
+        number = number * 2
+        label.text = String(number)
+    }
 
+    @IBAction func divid(){
+        number = number / 2
+        label.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
 }
 
